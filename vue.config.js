54e4ba7376175a2eb0a4722dc68a6c95fce229e8
KrystalY <yoosj0703@gmail.com>
2019-03-07 15:06:04 +0900

    add testing

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-// vue.config.js
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-  // options...
-};
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/testing/'
+      : '/',
+  outputDir: 'testing',
+  configureWebpack: {
+    resolve: {
+      modules: [
+        "node_modules",
+        path.resolve(__dirname, "app")
+      ],
+      alias: {
+        '@': path.resolve(__dirname, '/src'),
+      }
+    }
+  }
+}
